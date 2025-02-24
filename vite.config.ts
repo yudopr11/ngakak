78973189777e7f6ce14e2cd177da048ce4091cce
Yudho Prakoso <107313576+yudopr11@users.noreply.github.com>
2025-02-24 17:06:28 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,12 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  preview: {
+    port: 3000,
+    host: true,
+    allowedHosts: [
+      'localhost',
+      '.railway.app'
+    ]
+  }
 });
